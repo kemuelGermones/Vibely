c7b0be71142baab048c29ff52fe1cc076955c869
Kemuel Germones <kemuelgermones@gmail.com>
2023-07-04 15:04:48 +0800

    Add react query

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import { ModalContextProvider } from "./store/modal-context";
 
+const queryClient = new QueryClient();
 const appRoot = ReactDOM.createRoot(document.getElementById("app-root"));
 
 appRoot.render(
-  <ModalContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ModalContextProvider>
+  <QueryClientProvider client={queryClient}>
+    <ModalContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ModalContextProvider>
+  </QueryClientProvider>
 );
