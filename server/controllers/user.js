f599d72f675183a546b2e3ced3f2808a758e1119
Kemuel Germones <kemuelgermones@gmail.com>
2023-07-09 19:12:43 +0800

    Add a checker if a user does exist already

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,9 +4,16 @@ const admin = require("firebase-admin");
 
 const { User, Avatar } = require("../models");
 const sequelize = require("../config/sequelize");
+const AppError = require("../utils/AppError");
 
 module.exports.signup = async (req, res) => {
   const { firstname, lastname, username, email, password } = req.body;
+
+  const doesUserExist = await User.findOne({ where: { email }});
+
+  if (doesUserExist) {
+    throw new AppError("user exists already", 400);
+  }
 
   await sequelize.transaction(async (t) => {
     const user = await User.create(
