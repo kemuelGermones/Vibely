54882d230d033cd69c67fe5b68ea3f6c75916386
Kemuel Germones <kemuelgermones@gmail.com>
2023-07-09 17:41:22 +0800

    Change sign up auth to firebase

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,34 +2,32 @@ require("dotenv").config();
 
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const admin = require("firebase-admin");
 
 const { User, Avatar } = require("../models");
+const sequelize = require("../config/sequelize");
 const AppError = require("../utils/AppError");
 
 const SECRET = process.env.SECRET;
 const EXPIRATION = 60 * 60 * 24 * 7;
 
 module.exports.signup = async (req, res) => {
-  const doesUserExist = await User.findOne({
-    where: { email: req.body.email },
+  const { firstname, lastname, username, email, password } = req.body;
+
+  await sequelize.transaction(async (t) => {
+    const user = await User.create(
+      { firstname, lastname, username, email, password, avatar: req.file },
+      { include: [Avatar], transaction: t }
+    );
+
+    await admin.auth().createUser({
+      email,
+      password,
+      uid: user.id,
+    });
   });
 
-  if (doesUserExist) {
-    throw new AppError("user exists already", 400);
-  }
-
-  const user = await User.create(
-    { ...req.body, avatar: req.file },
-    { include: [Avatar] }
-  );
-
-  const token = jwt.sign({ id: user.id }, SECRET, {
-    expiresIn: EXPIRATION,
-  });
-
-  res
-    .status(200)
-    .json({ id, token: `Bearer ${token}`, expiration: EXPIRATION });
+  res.status(200).json({ status: 200, message: "successful sign up"})
 };
 
 module.exports.signin = async (req, res) => {
