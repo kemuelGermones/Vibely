07470e4cf7f29a62dc58c36ac63d6fc6b3863fe8
Kemuel Germones <kemuelgermones@gmail.com>
2023-07-13 20:30:45 +0800

    Remove bubble component

--- client/src/store/auth-context.js
+++ client/src/store/auth-context.js
@@ -2,7 +2,6 @@ import { useState, useEffect, createContext } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 
 import { auth } from "../config/firebase";
-import Bubble from "../components/ui/Bubble";
 
 export const AuthContext = createContext({
   user: null,
@@ -23,7 +22,11 @@ export function AuthContextProvider({ children }) {
     <AuthContext.Provider value={{ user }}>
       {user === undefined ? (
         <div className="fixed left-0 top-0 z-20 flex h-screen w-full items-center justify-center bg-yellow-200">
-          <Bubble />
+          <div className="flex items-center justify-center gap-3">
+            <div className="h-6 w-6 animate-[bounce_1s_infinite_-0.3s] rounded-full bg-yellow-400"></div>
+            <div className="h-6 w-6 animate-[bounce_1s_infinite_-0.1s] rounded-full bg-yellow-400"></div>
+            <div className="h-6 w-6 animate-[bounce_1s_infinite_0.1s] rounded-full bg-yellow-400"></div>
+          </div>
         </div>
       ) : null}
       {children}
