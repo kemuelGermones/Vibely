d1e73c0c2370c21a1a044e9ee79d31c2209b938f
Kemuel Germones <kemuelgermones@gmail.com>
2023-07-26 18:16:25 +0800

    Remove delete post form border

--- client/src/components/post/DeletePostForm.js
+++ client/src/components/post/DeletePostForm.js
@@ -22,7 +22,7 @@ function DeletePostForm({ id }) {
   return (
     <div className="rounded-lg bg-white p-3 shadow">
       <div className="flex flex-col gap-3">
-        <div className="flex flex-col gap-3 rounded-lg border border-red-500 p-6">
+        <div className="flex flex-col gap-3 rounded-lg p-6">
           <img className="mx-auto h-14 w-14" src="./cross-circle.svg" />
           <h1 className="text-center text-xl text-gray-700">Are you sure?</h1>
           <p className="text-center text-gray-500">
