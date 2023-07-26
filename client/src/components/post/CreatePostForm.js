00f5d5e59193993d4dbc7f3e1312260eae8f2f4c
Kemuel Germones <kemuelgermones@gmail.com>
2023-07-26 15:47:13 +0800

    Change create post svg to image

--- client/src/components/post/CreatePostForm.js
+++ client/src/components/post/CreatePostForm.js
@@ -61,28 +61,7 @@ function CreatePostForm() {
           }`}
           htmlFor="images"
         >
-          <svg
-            className="mx-auto h-20 w-20"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              <path
-                d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15"
-                stroke="#fde047"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            </g>
-          </svg>
+          <img className="mx-auto h-14 w-14" src="./upload.svg" />
           <h1 className="text-center text-xl text-gray-700">
             {formik.values.images.length && !formik.errors.images
               ? `${formik.values.images.length} images chosen`
