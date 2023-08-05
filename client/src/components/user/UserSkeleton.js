d7e3a3f6b6e92edc66762ba02d876b8ddc0b3574
Kemuel Germones <kemuelgermones@gmail.com>
2023-08-05 16:53:05 +0800

    Update user skeleton class

--- client/src/components/user/UserSkeleton.js
+++ client/src/components/user/UserSkeleton.js
@@ -4,14 +4,14 @@ function UserSkeleton() {
       <div className="flex flex-col gap-3">
         <div className="flex items-center gap-3">
           <div className="h-10 w-10 shrink-0 animate-pulse rounded-full bg-gray-300" />
-          <div className="flex flex-col gap-1">
-            <div className="h-3.5 w-24 animate-pulse rounded-full bg-gray-300" />
-            <div className="h-3.5 w-32 animate-pulse rounded-full bg-gray-300" />
+          <div className="flex w-full flex-col gap-1">
+            <div className="h-3.5 w-1/4 animate-pulse rounded-full bg-gray-300" />
+            <div className="h-3.5 w-2/6 animate-pulse rounded-full bg-gray-300" />
           </div>
         </div>
         <div className="flex justify-around">
-          <div className="h-3.5 w-24 animate-pulse rounded-full bg-gray-300" />
-          <div className="h-3.5 w-24 animate-pulse rounded-full bg-gray-300" />
+          <div className="h-3.5 w-2/6 animate-pulse rounded-full bg-gray-300" />
+          <div className="h-3.5 w-2/6 animate-pulse rounded-full bg-gray-300" />
         </div>
       </div>
     </div>
