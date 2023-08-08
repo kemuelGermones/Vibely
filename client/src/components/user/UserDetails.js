da2b1ea5484582cf0bb4009140b03651f2f27f9d
Kemuel Germones <kemuelgermones@gmail.com>
2023-08-08 18:23:37 +0800

    Refactor user details component

--- client/src/components/user/UserDetails.js
+++ client/src/components/user/UserDetails.js
@@ -8,7 +8,7 @@ import UserButtonGroup from "./UserButtonGroup";
 import handleError from "../../utils/handleError";
 
 function UserDetails({ id }) {
-  const { user } = useAuth();
+  const { user } = useAuth()
 
   const { isLoading, isError, data } = useQuery({
     queryKey: ["users", id],
@@ -20,37 +20,37 @@ function UserDetails({ id }) {
     return <UserSkeleton />;
   }
 
-  if (!data.items) {
-    return <Navigate to="/error" />;
-  }
-
-  return (
-    <div className="rounded-lg bg-white p-3 shadow">
-      <div className="flex flex-col gap-3">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-3">
-            <div className="h-10 w-10 shrink-0 ">
-              <img
-                className="h-full w-full rounded-full object-cover"
-                src={data.items.avatar.url}
-              />
-            </div>
-            <div>
-              <div className="font-semibold">{data.items.username}</div>
-              <div className="text-sm text-gray-500">
-                {`${data.items.firstname} ${data.items.lastname}`}
+  if (data.items) {
+    return (
+      <div className="rounded-lg bg-white p-3 shadow">
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-3">
+              <div className="h-10 w-10 shrink-0 ">
+                <img
+                  className="h-full w-full rounded-full object-cover"
+                  src={data.items.avatar.url}
+                />
+              </div>
+              <div>
+                <div className="font-semibold">{data.items.username}</div>
+                <div className="text-sm text-gray-500">
+                  {`${data.items.firstname} ${data.items.lastname}`}
+                </div>
               </div>
             </div>
+            {user.uid === id ? <UserButtonGroup /> : null}
           </div>
-          {user.uid === id ? <UserButtonGroup /> : null}
-        </div>
-        <div className="flex justify-around">
-          <button>100 followers</button>
-          <button>100 following</button>
+          <div className="flex justify-around">
+            <button>100 followers</button>
+            <button>100 following</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <Navigate to="/error" />;
 }
 
 export default UserDetails;
