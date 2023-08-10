ecbdbcbbad1834573b2d1a299efbd40a7cb436cd
Kemuel Germones <kemuelgermones@gmail.com>
2023-08-11 01:00:18 +0800

    Change width of the warning sign and sort classes

--- client/src/components/comment/CommentError.js
+++ client/src/components/comment/CommentError.js
@@ -3,7 +3,10 @@ import { BsExclamationTriangle } from "react-icons/bs";
 function CommentError() {
   return (
     <div className="flex gap-3">
-      <BsExclamationTriangle className="shrink-0 text-red-500 w-10" size="1.5em" />
+      <BsExclamationTriangle
+        className="w-10 shrink-0 text-red-500"
+        size="1.5em"
+      />
       <div className="flex flex-col">
         <h1 className="font-semibold">Something went wrong</h1>
         <p>An error occured while trying to fetch posts.</p>
--- client/src/components/post/PostError.js
+++ client/src/components/post/PostError.js
@@ -4,7 +4,10 @@ function PostError() {
   return (
     <div className="rounded-lg bg-white p-3 shadow">
       <div className="flex gap-3">
-        <BsExclamationTriangle className="shrink-0 text-red-500" size="1.5em" />
+        <BsExclamationTriangle
+          className="w-10 shrink-0 text-red-500"
+          size="1.5em"
+        />
         <div className="flex flex-col">
           <h1 className="font-semibold">Something went wrong</h1>
           <p>An error occured while trying to fetch posts.</p>
