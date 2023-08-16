3035d63511be1fd35e57f7d658b67fe6d9cfc48f
Kemuel Germones <kemuelgermones@gmail.com>
2023-08-16 11:47:06 +0800

    Redesign error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -9,13 +9,13 @@ function Error() {
 
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-3">
-      <h1 className="text-9xl font-semibold text-gray-700">404</h1>
+      <h1 className="text-center font-shrikhand text-9xl text-gray-700">404</h1>
       <h2 className="text-center text-xl text-gray-700">Page not found</h2>
       <p className="text-center text-gray-500">
         Oops! We couldn't find the page you are looking for.
       </p>
       <button
-        className="rounded-lg bg-gray-500 px-4 py-2 text-sm font-semibold text-white shadow hover:bg-gray-600 focus:outline-none"
+        className="rounded-lg bg-gray-500 px-4 py-2 text-sm font-semibold text-white shadow hover:bg-gray-700 focus:outline-none"
         type="button"
         onClick={handleHomepage}
       >
