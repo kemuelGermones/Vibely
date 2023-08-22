3297ddb7f108678a35ba73455cc89236149d761c
Kemuel Germones <kemuelgermones@gmail.com>
2023-08-22 14:26:54 +0800

    Remove console log

--- client/src/stores/AuthContext.js
+++ client/src/stores/AuthContext.js
@@ -13,7 +13,6 @@ export function AuthContextProvider({ children }) {
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
       setClient({ user, initialized: true });
     });
 
