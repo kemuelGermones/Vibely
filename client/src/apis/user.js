0e246e58a2747d9ddf3c0120f2e1e2cb5ff28b05
Kemuel Germones <kemuelgermones@gmail.com>
2023-08-24 15:07:55 +0800

    Add follow and unfollow user request

--- client/src/apis/user.js
+++ client/src/apis/user.js
@@ -25,3 +25,21 @@ export const getUsers = async ({ page, search }) => {
 
   return response.data.items;
 };
+
+export const followUser = async (userId) => {
+  const user = auth.currentUser;
+  const token = await getIdToken(user);
+
+  await axios.post(`http://localhost:5000/users/${userId}/follows`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+export const unfollowUser = async (userId) => {
+  const user = auth.currentUser;
+  const token = await getIdToken(user);
+
+  await axios.delete(`http://localhost:5000/users/${userId}/follows`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
