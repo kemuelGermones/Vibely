490eef9b29ed88b21ae4ca74419d0d943bc39912
Kemuel Germones <kemuelgermones@gmail.com>
2023-08-28 14:30:58 +0800

    Remove capitalize function

--- client/src/utils/handleError.js
+++ client/src/utils/handleError.js
@@ -3,26 +3,17 @@ import { AxiosError } from "axios";
 import { toast } from "react-toastify";
 
 function handleError(error) {
-  const capitalize = (msg) => {
-    const reduced = msg.replace(/[^a-z0-9 ]/gi, "");
-    const firstChar = reduced.charAt(0).toUpperCase();
-    const remainingChar = reduced.slice(1);
-    return firstChar.concat(remainingChar);
-  };
-
   if (error instanceof FirebaseError) {
     const auth = error.code.split("/")[1];
-    const code = auth.replace(/-/g, " ");
-    const message = capitalize(code);
+    const message = auth.replace(/-/g, " ");
     toast.error(message);
     return;
   }
 
   if (error instanceof AxiosError) {
-    const response = error.response
+    const message = error.response
       ? error.response.data.message
       : error.message;
-    const message = capitalize(response);
     toast.error(message);
     return;
   }
