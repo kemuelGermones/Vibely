1e7541e55abca178d0bf90e047f334e3fd748253
Kemuel Germones <kemuelgermones@gmail.com>
2023-10-31 21:11:30 +0800

    Update firebase configs

--- client/src/configs/firebase.js
+++ client/src/configs/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN;
-const PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
-const STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET;
-const MESSAGING_SENDER_ID = process.env.REACT_APP_MESSAGING_SENDER_ID;
-const APP_ID = process.env.REACT_APP_APP_ID;
+const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+const AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+const PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+const APP_ID = process.env.REACT_APP_FIREBASE_APP_ID;
 
 const firebaseConfig = {
   apiKey: API_KEY,
